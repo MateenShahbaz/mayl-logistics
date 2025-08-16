9444364fadbe22920256c4464a5c3175db235924
Mateen Shahbaz <129575762+MateenShahbaz@users.noreply.github.com>
2025-08-16 03:43:58 -0700

    Adding new backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(require("cors")());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
