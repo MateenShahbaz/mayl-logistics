5914f7f6fe3e5dc364c68cf950dd93f98116de8c
Mateen Shahbaz <129575762+MateenShahbaz@users.noreply.github.com>
2025-08-16 03:50:05 -0700

    Minor changes

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
